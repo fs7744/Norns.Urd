bcfea28310f285919ae458bfc1731c408a8f93f2
Victor.X.Qu (g-mis.cncd02.Newegg) 42377 <victor.x.qu@newegg.com>
2020-11-07 10:37:09 +0800

    fix code smell

---
+++ src/Norns.Urd/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S3776:Cognitive Complexity of methods should not be too high", Justification = "<Pending>", Scope = "type", Target = "~T:Norns.Urd.TypeExtensions")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S3776:Cognitive Complexity of methods should not be too high", Justification = "<Pending>", Scope = "type", Target = "~T:Norns.Urd.EmitExtensions")]

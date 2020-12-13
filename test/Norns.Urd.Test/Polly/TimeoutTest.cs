6dd7a0431b323627a21168b116bfac05a5470185
victor.x.qu <fs7744@hotmail.com>
2020-12-13 12:58:24 +0800

    update test

--- test/Norns.Urd.Test/Polly/TimeoutTest.cs
+++ test/Norns.Urd.Test/Polly/TimeoutTest.cs
@@ -55,7 +55,7 @@ namespace Test.Norns.Urd.Polly
         {
             var sut = Mock();
             Assert.Equal(4, await sut.WaitAsync(4));
-            await Assert.ThrowsAsync<TaskCanceledException>(() => sut.WaitAsync(200));
+            await Assert.ThrowsAsync<TaskCanceledException>(() => sut.WaitAsync(500));
         }
 
         [Fact]
